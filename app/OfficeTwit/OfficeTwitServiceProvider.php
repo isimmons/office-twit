e245b2a1e6dc377a25d2d82183f02e71c0eea389
Ian Simmons <isimmons33@gmail.com>
2013-12-12 17:51:19 -0500

    initial commit

---
+++ app/OfficeTwit/OfficeTwitServiceProvider.php
@@ -0,0 +1,11 @@
+<?php namespace OfficeTwit;
+
+use Illuminate\Support\ServiceProvider;
+
+class OfficeTwitServiceProvider extends ServiceProvider {
+
+    public function register()
+    {
+        //get booty wit it!
+    }
+}
