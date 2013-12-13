a217aa3a1f2e55b0b349b722082afaff343f33c1
Ian Simmons <isimmons33@gmail.com>
2013-12-12 22:21:48 -0500

    add login route

--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,4 @@
 <?php
 
-Route::get('/', function()
-{
-    return View::make('home');
-});
+Route::get('/', ['as' => 'twits', 'uses' => 'TwitsController@index'])->before('auth');
+Route::get('login', ['as' => 'login', 'uses' => 'SessionsController@create']);
