d7eff8889ffdfd0d8027dbf3c9c278dd8711432a
Ian Simmons <isimmons33@gmail.com>
2013-12-13 11:59:07 -0500

    add login and logout routes

--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,9 @@
 <?php
 
 Route::get('/', ['as' => 'twits', 'uses' => 'TwitsController@index'])->before('auth');
+
 Route::get('login', ['as' => 'login', 'uses' => 'SessionsController@create']);
+Route::post('login', 'SessionsController@store')->before('csrf');
+
+Route::get('logout', ['as' => 'logout', 'uses' => 'SessionsController@destroy']);
+
