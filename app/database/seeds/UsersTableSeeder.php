d32499e46f00031a579cb73e857b9ec6e31f948f
Ian Simmons <isimmons33@gmail.com>
2013-12-13 15:54:33 -0500

    hash password

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -10,14 +10,14 @@ class UsersTableSeeder extends Seeder {
 		$users = [
                         [
                             'username' => 'john',
-                            'password' => '1234',
+                            'password' => Hash::make('1234'),
                             'email' => 'john@twitmail.com',
                             'created_at' => new DateTime,
                             'updated_at' => new DateTime
                         ],
                         [
                             'username' => 'sally',
-                            'password' => '1234',
+                            'password' => Hash::make('1234'),
                             'email' => 'sally@twitmail.com',
                             'created_at' => new DateTime,
                             'updated_at' => new DateTime
