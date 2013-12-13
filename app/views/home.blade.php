7c3f55c3858e83c185c8ab183417b1d9db65eed8
Ian Simmons <isimmons33@gmail.com>
2013-12-12 20:43:10 -0500

    rename hello.php to home.blade.php

---
+++ app/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+ 
+{{-- meta --}}
+@section('meta')
+    <meta name="robots" content="index, follow">
+    {{-- other meta --}}
+@stop
+ 
+ 
+{{-- content --}}
+@section('content')
+    <div class="col-md-8">
+        <p>CONTENT</p>
+    </div>
+@stop
