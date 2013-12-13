f596d849bdc65dd4c913b4c0f4f9a803a263e3c2
Ian Simmons <isimmons33@gmail.com>
2013-12-13 14:21:26 -0500

    test create and store methods

--- app/tests/unit/SessionsControllerTest.php
+++ app/tests/unit/SessionsControllerTest.php
@@ -14,4 +14,28 @@ class SessionsControllerTest extends TestCase {
         $this->assertTrue($this->client->getResponse()->isOk());
     }
 
+    /**
+     * Make sure the create method returns the login view
+     *
+     * @return void
+     **/
+    public function testCreateMethodReturnsCorrectView()
+    {
+        $response = $this->action('GET', 'SessionsController@create');
+        
+        $this->assertEquals($response->getContent(), View::make('sessions.create'));
+    }
+
+    /**
+    * Test the  store method creates a session
+    *
+    * @return void
+    **/
+    public function testStoreMethodCreatesSession()
+    {
+        $response = $this->action('POST', 'SessionsController@store', ['username' => 'john', 'password' => '1234']);
+    
+        
+    }
+
 }
