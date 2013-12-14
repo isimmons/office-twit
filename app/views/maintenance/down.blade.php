8b811ed4910cea2e68b2be53f918d238e2d34228
Ian Simmons <isimmons33@gmail.com>
2013-12-14 14:31:40 -0500

    silly miley wrecked the site (maintenance mode view)

---
+++ app/views/maintenance/down.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Down For Maintenance</title>
+    
+</head>
+<body>
+    <h1>Down For Maintenance</h1>
+    <p>
+        Sorry We are down for maintanance. This is temporary so try again soon!
+    </p>
+    <p>
+        In the meantime here is some entertainment for your viewing pleasure :-)
+    </p>
+    <img src="img/miley.jpg">
+</body>
+</html>
