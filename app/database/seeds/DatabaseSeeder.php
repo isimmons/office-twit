86ccbbee5bc009711276d4ec58d155e85781ade1
Ian Simmons <isimmons33@gmail.com>
2013-12-31 15:27:49 -0500

    set foreign key constraints to false before truncating

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -10,13 +10,14 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		Eloquent::unguard();
-
+		DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 		DB::table('users')->truncate();
 		DB::table('twits')->truncate();
 		
 		
 		$this->call('UsersTableSeeder');
 		$this->call('TwitsTableSeeder');
+		DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 		
 	}
 
