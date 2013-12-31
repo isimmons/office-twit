e77a34cf0fce8ccc8dbac92f4c5e97a70f80b9ef
Ian Simmons <isimmons33@gmail.com>
2013-12-31 15:07:20 -0500

    minor change to DateTime

--- app/database/seeds/TwitsTableSeeder.php
+++ app/database/seeds/TwitsTableSeeder.php
@@ -8,8 +8,8 @@ class TwitsTableSeeder extends Seeder {
 		DB::table('twits')->truncate();
 
 		$twits = [
-                        ['twit' => 'Johns first twit', 'user_id' => 1, 'created_at' => new DateTime(), 'updated_at' => new DateTime()],
-                        ['twit' => 'Sallys first twit', 'user_id' => 2, 'created_at' => new DateTime(), 'updated_at' => new DateTime()]
+                        ['twit' => 'Johns first twit', 'user_id' => 1, 'created_at' => new DateTime, 'updated_at' => new DateTime],
+                        ['twit' => 'Sallys first twit', 'user_id' => 2, 'created_at' => new DateTime, 'updated_at' => new DateTime]
                     ];
 
 		// Uncomment the below to run the seeder
