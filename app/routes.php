3851714817201e127e42e20924ce5596bed90ffa
Ian Simmons <isimmons33@gmail.com>
2013-12-31 14:03:53 -0500

    move all auth routes to route group

--- app/routes.php
+++ app/routes.php
@@ -1,13 +1,19 @@
 <?php
 
-Route::get('/', ['as' => 'twits', 'uses' => 'TwitsController@index'])->before('auth');
+Route::group(['before' => 'auth'], function()
+{
+    Route::get('/', ['as' => 'twits', 'uses' => 'TwitsController@index']);
+
+    Route::get('twits', 'TwitsController@index');
+
+    Route::get('users', 'UsersController@index');
+
+    Route::get('profile', 'UsersController@profile');
+});
+
+
 
 Route::get('login', ['as' => 'login', 'uses' => 'SessionsController@create']);
 Route::post('login', 'SessionsController@store')->before('csrf');
 Route::get('logout', ['as' => 'logout', 'uses' => 'SessionsController@destroy']);
 
-Route::get('twits', 'TwitsController@index')->before('auth');
-
-Route::get('users', 'UsersController@index')->before('auth');
-
-Route::get('profile', 'UsersController@profile')->before('auth');
