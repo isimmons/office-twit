bc2757e6f20d6a86605b752becb4fa7ba2b18ea0
Ian Simmons <isimmons33@gmail.com>
2014-01-17 22:26:01 -0500

    remove profile settings composer, nothing to register

--- app/OfficeTwit/OfficeTwitServiceProvider.php
+++ app/OfficeTwit/OfficeTwitServiceProvider.php
@@ -1,11 +1,12 @@
 <?php namespace OfficeTwit;
 
 use Illuminate\Support\ServiceProvider;
+use View;
 
 class OfficeTwitServiceProvider extends ServiceProvider {
 
     public function register()
     {
-        //get booty wit it!
+        //nothing to register at this point
     }
 }
