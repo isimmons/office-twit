b4f60d1ea84e68af7c6c61f2cc865dbcd3d6f651
Ian Simmons <isimmons33@gmail.com>
2014-01-17 22:26:20 -0500

    some testing and cleanup

--- app/routes.php
+++ app/routes.php
@@ -20,4 +20,3 @@ Route::group(['before' => 'auth'], function()
 Route::get('login', ['as' => 'login', 'uses' => 'SessionsController@create']);
 Route::post('login', 'SessionsController@store')->before('csrf');
 Route::get('logout', ['as' => 'logout', 'uses' => 'SessionsController@destroy']);
-
