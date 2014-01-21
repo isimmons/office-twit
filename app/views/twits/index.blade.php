1588a1bd8548ab8187c52a99201be4801e8ba649
Ian Simmons <isimmons33@gmail.com>
2014-01-20 19:21:12 -0500

    add sidebar and some markup

--- app/views/twits/index.blade.php
+++ app/views/twits/index.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.master')
 
 @section('content')
-    <ul>
-        @foreach($twits as $twit)
-            <li>
-                {{ $twit->twit }}
-            </li>
-        @endforeach
-    </ul>    
+    <div class="col-md-6 timeline">
+         @include('twits.partials._timelineSideBar')
+    </div>
+
+    <div class="col-md-6 timeline">
+        <h2>Public Twits</h2>
+        <ul class="well">
+            @foreach($twits as $twit)
+                <li>{{ $twit->twit }}</li>
+            @endforeach
+        </ul>
+    </div>
 @stop
