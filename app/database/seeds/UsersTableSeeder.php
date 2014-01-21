3ed3c11389b79b320d4ed4095ba53360b227d4c0
Ian Simmons <isimmons33@gmail.com>
2014-01-21 10:42:23 -0500

    add defaultSettings method for UsersTableSeeder, reset migrations and seed

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -11,6 +11,7 @@ class UsersTableSeeder extends Seeder {
                             'username' => 'john',
                             'password' => Hash::make('1234'),
                             'email' => 'john@twitmail.com',
+                            'settings' => $this->getDefaultSettings(),
                             'created_at' => new DateTime,
                             'updated_at' => new DateTime
                         ],
@@ -18,6 +19,7 @@ class UsersTableSeeder extends Seeder {
                             'username' => 'sally',
                             'password' => Hash::make('1234'),
                             'email' => 'sally@twitmail.com',
+                            'settings' => $this->getDefaultSettings(),
                             'created_at' => new DateTime,
                             'updated_at' => new DateTime
                         ]
@@ -25,5 +27,17 @@ class UsersTableSeeder extends Seeder {
 		
 		DB::table('users')->insert($users);
 	}
+
+        protected function getDefaultSettings()
+        {
+            $settings = [
+                'allowTwitter' => '0',
+                'twitterHandle' => null,
+                'publicEmail' => false
+            ];
+
+            return json_encode($settings);
+            
+        }
 
 }
