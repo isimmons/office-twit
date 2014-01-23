45396d3aaed9f87fbea7715d513a6f1fba9c28d1
Ian Simmons <isimmons33@gmail.com>
2014-01-23 12:18:31 -0500

    add company gravitar setting, some comments

--- app/config/officeTwit.php
+++ app/config/officeTwit.php
@@ -1,6 +1,10 @@
 <?php
 return [
 
+    //globally allow users to show their public twitter timefeed (true/false)
     'allowTwitter' => true,
+
+    //set a company gravitar email or leave blank to use gravitar.com default image
+    'defaultGravitarEmail' => '',
     
 ];
