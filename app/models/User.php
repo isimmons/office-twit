a88e711dd92ba9e8486ac1ef6a94883b1dda3992
Ian Simmons <isimmons33@gmail.com>
2014-01-25 15:19:19 -0500

    add user_follows table and user follow relationships

--- app/models/User.php
+++ app/models/User.php
@@ -5,73 +5,91 @@ use Illuminate\Auth\Reminders\RemindableInterface;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
 
-	/**
-	 * Mass assignment protection.
-	 *
-	 * @var array
-	 */
-	protected $fillable = ['username', 'email', 'password', 'settings'];
+    /**
+     * Mass assignment protection.
+     *
+     * @var array
+     */
+    protected $fillable = ['username', 'email', 'password', 'settings'];
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password');
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = array('password');
 
-	/**
-	 * Get the unique identifier for the user.
-	 *
-	 * @return mixed
-	 */
-	public function getAuthIdentifier()
-	{
-		return $this->getKey();
-	}
+    /**
+     * Get the unique identifier for the user.
+     *
+     * @return mixed
+     */
+    public function getAuthIdentifier()
+    {
+        return $this->getKey();
+    }
 
-	/**
-	 * Get the password for the user.
-	 *
-	 * @return string
-	 */
-	public function getAuthPassword()
-	{
-		return $this->password;
-	}
+    /**
+     * Get the password for the user.
+     *
+     * @return string
+     */
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
 
-	/**
-	 * Get the e-mail address where password reminders are sent.
-	 *
-	 * @return string
-	 */
-	public function getReminderEmail()
-	{
-		return $this->email;
-	}
+    /**
+     * Get the e-mail address where password reminders are sent.
+     *
+     * @return string
+     */
+    public function getReminderEmail()
+    {
+        return $this->email;
+    }
 
-	/**
-	 * Force hashing of passwords.
-	 *
-	 * @param string $password
-	 * @return void
-	 */
-	public function setPasswordAttribute($value)
-	{
-		//don't rehash already hashed password when user updates profile
-		if(Hash::needsRehash($value))
-			$this->attributes['password'] = Hash::make($value);
-	}
+    /**
+     * Force hashing of passwords.
+     *
+     * @param string $password
+     * @return void
+     */
+    public function setPasswordAttribute($value)
+    {
+        //don't rehash already hashed password when user updates profile
+        if(Hash::needsRehash($value))
+            $this->attributes['password'] = Hash::make($value);
+    }
 
-	public function twits()
-	{
-		return $this->hasMany('Twit');
-	}
+    public function twits()
+    {
+        return $this->hasMany('Twit');
+    }
+
+    /**
+    * User following relationship
+    */
+    public function follow()
+    {
+        return $this->belongsToMany('User', 'user_follows', 'user_id', 'follow_id');
+    }
+
+    /**
+    * User followers relationship
+    */
+    public function followers()
+    {
+        return $this->belongsToMany('User', 'user_follows', 'follow_id', 'user_id');
+    }
+
+
 
 }
